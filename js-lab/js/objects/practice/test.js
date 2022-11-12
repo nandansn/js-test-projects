5b0fbbc2c09614639c1cf2e4d79b62728f4c90cb
nandansn <geeknanda13@gmail.com>
2022-11-11 20:12:38 -0800

    updated

---
+++ js-lab/js/objects/practice/test.js
@@ -0,0 +1,28 @@
+// object literal
+
+let empty_object = {}
+
+let new_object = {}
+
+console.log(empty_object);
+console.log(new_object);
+
+// add preoperty
+
+empty_object.name = 'nanda'
+empty_object.age = 20
+
+
+console.log(empty_object);
+
+// property name with special chars
+
+let person =  {
+    ["first_name"]:"nanda"
+}
+
+console.log(person["first_name"]);
+
+
+
+
--- js-lab/js/objects/test.js
+++
@@ -1,7 +0,0 @@
-// object literal
-
-let empty_object = {}
-
-let new_object = {}
-
-console.log(empty_object);
