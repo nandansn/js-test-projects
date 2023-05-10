8e1b2ac5d00f1596aadfbe8f1f384200dc3ff19a
nandakumar.rangasamy <geeknanda13@gmail.com>
2023-05-10 11:16:00 +0530

    update

---
+++ js-solid/dependencyinjection.js
@@ -0,0 +1,3 @@
+/**
+ *  Dependency Injection is an application of IoC, i.e., Inversion of Control. Technically speaking, Inversion of Control is the programming principle, that says invert the control of the program flow.
+ */
---
+++ js-solid/singleresponsibilityprinciple.js
@@ -0,0 +1,5 @@
+/**
+ * A class, method or function should undertake the responsibility of one functionality. In simpler words, it should carry out only one feature/functionality.
+ * 
+ * In OOPs paradigm, one class should only serve one purpose. This does not mean that each class should have just one method, but the methods you define inside a class should be related to the responsibility of that class.
+ */
