c7066183c8d1ef38281511cc2d6deca95f55b5e7
nandakumar.rangasamy <geeknanda13@gmail.com>
2023-05-29 21:34:04 +0530

    updated

---
+++ js-interview-400/practice/20.js
@@ -0,0 +1,15 @@
+switch (key) {
+    
+    case '1':
+        let count = 1;
+        break;
+
+    case '2':
+        {
+        let count = 2;
+        break;
+        }
+
+    default:
+        break;
+}
